53821d3a4f55dea08ad6636767a2ad9c25bde8a3
Amirreza <Amirreza@AMIRREZA>
2021-10-18 19:31:38 +0330

    Rename the class library folder named 'AT-Notepad.WFA.NetCore.Infrastructure'

---
+++ [Windows Form Application]/AT-Notepad.WFA.NetCore.Infrastructure/First_Case/CONST.cs
@@ -0,0 +1,7 @@
+﻿namespace AT_Notepad.WFA.NetCore.Infrastructure.First_Case
+{
+    public static class CONST
+    {
+        public const string CannotFindMessage = "Cannot find \"{SearchText}\"";
+    }
+}
